59862fe00d9957321eab7bbf86ae5012db2ec72c
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-26 00:08:55 -0500

    update dependancies

--- Client/postcss.config.cjs
+++ Client/postcss.config.cjs
@@ -1,16 +1,10 @@
+// Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
+// it will create an unexpected error: Invalid PostCSS Plugin found: [0]
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: {
-      preset: [
-        "default",
-        {
-          discardComments: {
-            removeAll: true,
-          },
-        },
-      ],
-    },
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
--- Client/tailwind.config.cjs
+++ Client/tailwind.config.cjs
@@ -1,51 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,md,html}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx,md,html}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     container: {
       padding: {
-        DEFAULT: ".9rem",
-        lg: "2.5rem",
+        DEFAULT: '.9rem',
+        lg: '2.5rem',
       },
     },
     screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      xxl: "1536px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
     },
     extend: {
       colors: {
         primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
+          100: '#212121',
         },
-        main: "#49fdd8",
-        background: "#212121",
-        tertiary: "#34055b",
+        main: '#49fdd8',
+        background: '#212121',
+        tertiary: '#34055b',
         accent: {
-          DEFAULT: "#00d3a7",
-          hover: "#00d3a7",
+          DEFAULT: '#00d3a7',
+          hover: '#00d3a7',
         },
-        paragraph: "#D1D1D1",
+        paragraph: '#D1D1D1',
       },
     },
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("prettier-plugin-tailwindcss"),
-    require("flowbite/plugin"),
+    require('@tailwindcss/forms'),
+    require('prettier-plugin-tailwindcss'),
+    require('flowbite/plugin'),
   ],
 };
